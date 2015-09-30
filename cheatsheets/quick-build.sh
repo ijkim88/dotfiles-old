7a75ae1d62f8d9ec58c50a074aa3cc44ea5ad6ae
Isaac Kim <ijkim88@gmail.com>
2015-09-30 14:33:09 -0700

    Update cheatsheet quick-build script to detect Python version

--- cheatsheets/quick-build.sh
+++ cheatsheets/quick-build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-cd ~/dotfiles/cheatsheets/ && make html && cd build/html/ && python -m SimpleHTTPServer
+PYTHON_VER=$(python --version 2>&1)
+
+if [ "${PYTHON_VER:7:1}" == '2' ]; then
+    cd ~/dotfiles/cheatsheets/ && make html && cd build/html/ && python -m SimpleHTTPServer
+elif [ "${PYTHON_VER:7:1}" == '3' ]; then
+    cd ~/dotfiles/cheatsheets/ && make html && cd build/html/ && python -m http.server
+else
+    >&2 echo "Unknown Python version. Please check to see if Python is installed."
+    exit 1;
+fi
