1188aad34057929d79f204a9e31fdf5bd70e776f
Isaac Kim <ijkim88@gmail.com>
2016-01-18 12:28:56 -0800

    Update cheatsheet quick-build script to automatically use port 8080

--- cheatsheets/quick-build.sh
+++ cheatsheets/quick-build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+source ~/virtualenv/sphinx/bin/activate
+
 PYTHON_VER=$(python --version 2>&1)
 
 if [ "${PYTHON_VER:7:1}" == '2' ]; then
-    cd ~/dotfiles/cheatsheets/ && make html && cd build/html/ && python -m SimpleHTTPServer
+    cd ~/dotfiles/cheatsheets/ && make html && cd build/html/ && python -m SimpleHTTPServer 8080
 elif [ "${PYTHON_VER:7:1}" == '3' ]; then
-    cd ~/dotfiles/cheatsheets/ && make html && cd build/html/ && python -m http.server
+    cd ~/dotfiles/cheatsheets/ && make html && cd build/html/ && python -m http.server 8080
 else
     >&2 echo "Unknown Python version. Please check to see if Python is installed."
     exit 1;
